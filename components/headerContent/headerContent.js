77132647374559f75211ed89a4b6d43f2cb64e5a
Agrawal, Arpit (US - Mumbai) <arpagrawal@deloitte.com>
2017-09-08 16:53:41 +0530

    final header changes

--- components/headerContent/headerContent.js
+++ components/headerContent/headerContent.js
@@ -1,9 +1,18 @@
 function HeaderContentController() {
 	var self= this;
-	self.activeItem = 'Home';
+	self.activeItem = 'Home'; //Current active Nav
+
+	//navItems array
 	self.navItems = ["Home","About Us","Discover","FAQ"];
+
+	//function to select current active nav
 	self.isActive = function(item){
 		self.activeItem = item;
+	}
+
+	//Function for header button click
+	self.clicked = function(){
+		alert("Button Clicked");
 	}
 }
 

254d943961b90c64bed278c96bf363ed021f4008
DimpleR <drudakia@deloitte.com>
2017-09-08 17:01:11 +0530

    panel component
    panel component

--- components/mainContent/mainContent.js
+++ components/mainContent/mainContent.js
@@ -1,10 +1,10 @@
 function MainContentController() {
   this.panels=
       [
-        {"imageUrl":"./images/panel_image1.png","name":"Lorem ipsum","title":"fe developer"},
-        {"imageUrl":"./images/panel_image2.jpg","name":"Lorem ipsum","title":"visual developer"},
-        {"imageUrl":"./images/panel_image3.png","name":"Lorem ipsum","title":"visual developer"},
-        {"imageUrl":"./images/panel_image4.png","name":"Lorem ipsum","title":"fe developer"}
+        {"imageUrl":"./images/panel_image1.png","name":"Lorem ipsum","title":"FE Developer"},
+        {"imageUrl":"./images/panel_image2.jpg","name":"Lorem ipsum","title":"Visual Developer"},
+        {"imageUrl":"./images/panel_image3.png","name":"Lorem ipsum","title":"Visual Developer"},
+        {"imageUrl":"./images/panel_image4.png","name":"Lorem ipsum","title":"FE Developer"}
       ];
 
 }

7648f9201df77cabf7e1f6b1e46fdce6542aadbd
Agrawal, Arpit (US - Mumbai) <arpagrawal@deloitte.com>
2017-09-08 16:40:52 +0530

    header changes by arpit

--- components/headerContent/headerContent.js
+++ components/headerContent/headerContent.js
@@ -1,10 +1,16 @@
 function HeaderContentController() {
-
+	var self= this;
+	self.activeItem = 'Home';
+	self.navItems = ["Home","About Us","Discover","FAQ"];
+	self.isActive = function(item){
+		self.activeItem = item;
+	}
 }
 
 angular.module('ssApp').component('headerContent', {
   templateUrl: './components/headerContent/headerContent.html',
   controller: HeaderContentController,
+  controllerAs: 'headCtrl',
   bindings: {
   }
 });

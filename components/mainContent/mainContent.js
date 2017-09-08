225988c626abc42f195ddf0027933da24411be48
DimpleR <drudakia@deloitte.com>
2017-09-08 16:01:45 +0530

    panel compoennt change
    panel compoennt change

--- components/mainContent/mainContent.js
+++ components/mainContent/mainContent.js
@@ -1,10 +1,18 @@
 function MainContentController() {
+  this.panels=
+      [
+        {"name":"Lorem ipsum","title":"fe developer"},
+        {"name":"Lorem ipsum","title":"visual developer"},
+        {"name":"Lorem ipsum","title":"visual developer"},
+        {"name":"Lorem ipsum","title":"fe developer"}
+      ];
 
 }
 
 angular.module('ssApp').component('mainContent', {
   templateUrl: './components/mainContent/mainContent.html',
   controller: MainContentController,
+  controllerAs: 'mainctrl',
   bindings: {
   }
 });
